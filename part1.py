87dc2ef5432bce87bc5a2b39eea6265241f00490
Denis <den2295404@gmail.com>
2024-07-03 02:54:18 +0300

    Update part1.py

--- part1.py
+++ part1.py
@@ -1,8 +1,13 @@
 import pandas as pd
 
 
-df = pd.read_csv("recipes.csv")
-print(df.head(10))
+df0 = pd.read_csv("recipes.csv")
+print(df0.head(3))
+print(df0.tail(1))
+
+df1=pd.DataFrame(df0)
+print(df1.head())
+print(df1.tail())
 
 def read_csv(file):
     return pd.read_csv(file)
