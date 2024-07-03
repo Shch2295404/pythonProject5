aac2f6484009f9fcf2786b9ee1429ebe58bfe3a1
Denis <den2295404@gmail.com>
2024-07-03 05:54:04 +0300

    Update part4.py

--- part4.py
+++ part4.py
@@ -6,10 +6,12 @@ print(df.head())
 print(df.info())  # печатает информацию о DataFrame
 print(df.describe())  # печатает описательные статистики
 
+df.fillna(0, inplace=True)  # inplace=True - обновляет df вместе с заменой пропущенных значений
+print(df)
+
 '''
  группирует DataFrame по столбцу City и считает среднее значение в столбце Salary
 '''
 grouped_df = df.groupby('City')['Salary'].mean()
-
-print(df.head(len(grouped_df)))
 print(grouped_df)
+print(grouped_df.agg(['min', 'max'])) # печатает минимальное и максимальное значение для каждого города
