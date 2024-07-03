214adf9a75e123b921b5056f9a93ffc80c57b8c9
Denis <den2295404@gmail.com>
2024-07-03 05:19:37 +0300

    Update part3.py

--- part3.py
+++ part3.py
@@ -1,11 +1,12 @@
 import pandas as pd
-from pandas import DataFrame
 
 
 df = pd.read_csv('animal.csv')
 print(df.head(len(df)))
 
-
+df.fillna(0, inplace=True) # inplace=True - обновляет df вместе с заменой пропущенных значений
 print(df.head(len(df)))
 
+'''
 df.to_csv('animal.csv', index=False)
+'''
