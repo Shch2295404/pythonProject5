83c1e0bce4f83417ac3db84b1da11a0fa19fd805
Denis <den2295404@gmail.com>
2024-07-03 05:24:55 +0300

    Update part3.py

--- part3.py
+++ part3.py
@@ -1,12 +1,13 @@
 import pandas as pd
 
-
 df = pd.read_csv('animal.csv')
 print(df.head(len(df)))
 
-df.fillna(0, inplace=True) # inplace=True - обновляет df вместе с заменой пропущенных значений
+df.fillna(0, inplace=True)  # inplace=True - обновляет df вместе с заменой пропущенных значений
 print(df.head(len(df)))
+'''
+df.dropna(inplace=True) # inplace=True - обновляет df вместе с удалением пропущенных значений
+print(df.head(len(df)))
+'''
 
-'''
-df.to_csv('animal.csv', index=False)
-'''
+df.to_csv('animal.csv', index=False)  # index=False - не сохраняет индексы
