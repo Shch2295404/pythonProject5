9c31864d6032df77e427e7c1b7c469495ffe16b2
Denis <den2295404@gmail.com>
2024-07-07 22:08:54 +0300

    Update az03_2.py

--- az03_2.py
+++ az03_2.py
@@ -2,14 +2,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-# Generate two sets of random data
+# Генерируем два набора случайных данных
 num_points = 100
 x_data = np.random.rand(num_points)
 y_data = np.random.rand(num_points)
 
-# Create a scatter plot
+# Создаем диаграмму рассеивания
 plt.scatter(x_data, y_data, c='blue', alpha=0.5)
-plt.title('Scatter Plot of Random Data')
+plt.title('Диаграмма рассеивания случайных данных')
 plt.xlabel('X Data')
 plt.ylabel('Y Data')
 plt.show()
