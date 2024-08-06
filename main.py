ed4ba498f87681eca5bb5bbbe584518462858749
Denis <den2295404@gmail.com>
2024-08-07 00:50:45 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,5 +3,12 @@ from aiogram import Bot, Dispatcher
 
 from config import TOKEN
 
-bot = Bot(token=TOKEN)
-dp = Dispatcher(bot)
+bot = Bot(token='')
+dp = Dispatcher()
+
+
+async def main():
+    await dp.start_polling()
+
+if __name__ == "__main__":
+    asyncio.run(main())
