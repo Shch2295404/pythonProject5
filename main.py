20b8fe27595b6682c3a23a514d9dfb77780037bd
Denis <den2295404@gmail.com>
2024-08-07 23:16:08 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 import asyncio
 from aiogram import Bot, Dispatcher
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+
+
+async def echo(message: Message):
+    await message.answer(message.text)
+
+
+async def on_startup(dp):
+    print('Бот запущен')
 
 
 bot = Bot(token='')
@@ -8,6 +18,7 @@ dp = Dispatcher()
 
 async def main():
     await dp.start_polling()
+
 
 if __name__ == "__main__":
     asyncio.run(main())
