1aede27445a6acedeb64393714ee069cd36b9da5
Denis <den2295404@gmail.com>
2024-08-07 23:24:18 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,17 +3,13 @@ from aiogram import Bot, Dispatcher
 from aiogram.filters import CommandStart
 from aiogram.types import Message
 
-
-async def echo(message: Message):
-    await message.answer(message.text)
-
-
-async def on_startup(dp):
-    print('Бот запущен')
-
-
 bot = Bot(token='')
 dp = Dispatcher()
+
+
+@dp.message(CommandStart)
+async def start(message: Message):
+    await message.answer(f'Привет, {message.from_user.full_name}!')
 
 
 async def main():
